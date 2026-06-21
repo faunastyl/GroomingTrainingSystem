d7f212382f11cbcd5dc88ae4722bf64026c13d3b
Bożena <faunastyl@interia.pl>
2026-06-22 00:29:03 +0200

    Added User abstraction and Subscriber with Observer pattern implementation

---
+++ Subscriber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Subscriber : User
+{
+    public Subscriber(string email) : base(email)
+    {
+    }
+
+    public override void Notify(Training training)
+    {
+        Console.WriteLine($"[POWIADOMIENIE] {Email}: Nowe szkolenie -> {training.Title} w {training.City} ({training.Date.ToShortDateString()})");
+    }
+}
---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public abstract class User
+{
+    public string Email { get; set; }
+
+    public User(string email)
+    {
+        Email = email;
+    }
+
+    // POLIMORFIZM (każdy typ użytkownika zachowuje się inaczej)
+    public abstract void Notify(Training training);
+}
