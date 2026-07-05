390593132fb9a26dfc5a7d04158ba9cbdc8ffeef
Bożena <faunastyl@interia.pl>
2026-07-05 08:42:45 +0200

    Added Location model for future training venues

---
+++ Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Location
+{
+    public string City { get; set; }
+    public string Address { get; set; }
+
+    public Location(string city, string address)
+    {
+        City = city;
+        Address = address;
+    }
+
+    public override string ToString()
+    {
+        return $"{City}, {Address}";
+    }
+}
